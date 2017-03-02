163810faa3600abc898294cf677f508a983ae322
flavkupe <flavkupe@umich.edu>
2017-03-02 02:04:45 -0800

    More levels and mechanics

---
+++ Assets/Scripts/ColorLockZone.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorLockZone : ColorBlockingZone
+{
+
+    public void SetOpen(bool open)
+    {
+        boundingBox.isTrigger = open;
+        EnabledMeshes.SetActive(!open);
+        DisabledMeshes.SetActive(open);
+    }
+
+    public GameObject EnabledMeshes;
+    public GameObject DisabledMeshes;
+
+    public Collider2D boundingBox;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/ColorLockZoneTrigger.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorLockZoneTrigger : MonoBehaviour {
+
+    public ColorLockZone[] Zones;
+
+    public BlockColor Color;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Block block = collider.GetComponent<Block>();
+        if (block != null && block.Color == this.Color)
+        {
+            ToggleZones(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        Block block = collider.GetComponent<Block>();
+        if (block != null && block.Color == this.Color)
+        {
+            ToggleZones(false);
+        }
+    }
+
+    private void ToggleZones(bool open)
+    {
+        if (this.Zones != null && this.Zones.Length > 0)
+        {
+            foreach (ColorLockZone zone in this.Zones)
+            {
+                zone.SetOpen(open);
+            }
+        }
+    }
+    
+}
